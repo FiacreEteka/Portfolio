46300313835954e66c09c25f0609e382eab2d6dd
ETEKA Fiacre <fiacreeteka213@gmail.com>
2025-06-13 19:39:15 +0100

    Modification du formulaire

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,10 +1,12 @@
-function verifier() {
+function verifier(event) {
+    
     let nom = document.querySelector(".nom").value;
     let prenom = document.querySelector(".prenom").value;
     let message = document.querySelector(".message").value
     let sujet = document.querySelector(".sujet").value
     
     if(nom ==="" || message ==="" || prenom ==="" || sujet ==="") {
+        event.preventDefault();
         alert("Veuilez remplir tous les champs")
     }else {
         alert("Message envoyé")
